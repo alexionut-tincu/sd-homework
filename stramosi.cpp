ce941dea21b973188b9331651473440a2ed8b361
alexionut05 <alexionut05@users.noreply.github.com>
2024-05-30 05:44:17 +0300

    Refactor file input/output in stramosi.cpp

--- stramosi.cpp
+++ stramosi.cpp
@@ -2,57 +2,46 @@
 
 #define SIZE 250001
 
-template<typename T>
-void fastscan(T &number, FILE *file = stdin)
-{
-	bool negative = false;
-	char c;
-	number = 0;
-	c = getc(file);
-	if (c == '-')
-	{
-		negative = true;
-		c = getc(file);
-	}
-	for (; (c > 47 && c < 58); c = getc(file)) {
-		number = number * 10 + c - 48;
-	}
-	if (negative) {
-		number *= -1;
-	}
-}
-
 int main()
 {
-	FILE *fin = fopen("stramosi.in", "r");
+	std::ifstream fin("stramosi.in", std::ios::binary);
 	std::ofstream fout("stramosi.out");
 
+	std::streambuf* raw_buffer = fin.rdbuf();
+
+   
+	fin.seekg(0, std::ios::end);
+	std::streamsize size = fin.tellg();
+	fin.seekg(0, std::ios::beg);
+
+	char *block = new char[size];
+	raw_buffer->sgetn(block, size);
+	char *ptr = block;
+
 	int64_t members, queries;
-	fastscan(members, fin);
-	fastscan(queries, fin);
+	members = std::strtoll(ptr, &ptr, 10);
+	queries = std::strtoll(ptr, &ptr, 10);
 
 	int64_t tree_depth = 0;
 
 	int64_t ancestors[SIZE];
 	for (int64_t i = 1; i <= members; ++i) {
-		fastscan(ancestors[i], fin);
+		ancestors[i] = std::strtoll(ptr, &ptr, 10);
 		tree_depth += (ancestors[i] == i - 1);
 	}
 
 	if (tree_depth == members) {
 		while (queries--) {
-			int64_t x, y;
-			fastscan(x, fin);
-			fastscan(y, fin);
+			int64_t x = std::strtoll(ptr, &ptr, 10);
+            		int64_t y = std::strtoll(ptr, &ptr, 10);
 			fout << ((x >= y) ? (x - y) : 0) << '\n';
 		}
 	}
 
 	else {
 		while (queries--) {
-			int64_t x, y;
-			fastscan(x, fin);
-			fastscan(y, fin);
+			int64_t x = std::strtoll(ptr, &ptr, 10);
+            		int64_t y = std::strtoll(ptr, &ptr, 10);
 
 			while (y-- && x) {
 				x = ancestors[x];
@@ -61,6 +50,8 @@ int main()
 			fout << x << '\n';
 		}
 	}
+
+	delete[] block;
 
 	return 0;
 }
